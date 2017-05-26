89a1b4bf2302b59fb5309b0f3c8101a64f92b5db
James Goodman <james@janigo.co.uk>
2017-05-26 18:47:43 +0100

    CHAPEL now converted to use main decision cycle
    Tests green.

--- src/hopshackle/dominion/basecards/Chapel.java
+++ src/hopshackle/dominion/basecards/Chapel.java
@@ -6,8 +6,14 @@ import hopshackle.simulation.Action;
 import hopshackle.simulation.ActionEnum;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Chapel extends Card {
+
+	private static AtomicLong idFountain = new AtomicLong(1);
+
+	private int trashedSoFar;
+	private Player player;
 
 	public Chapel() {
 		super(CardType.CHAPEL);
@@ -15,27 +21,100 @@ public class Chapel extends Card {
 
 	@Override
 	public List<ActionEnum<Player>> takeAction(Player player) {
-		for (int loop = 0; loop < 4; loop ++)
-			trashCard(player);
-		return emptyList;
+		super.takeAction(player);
+		this.player = player;
+		this.setRef("CHAPEL" + idFountain.getAndIncrement());
+		// We can trash any one card from hand, or NONE
+		// We can do this a total of four times
+		// So the follow-on action can simply be a brand new action, referencing the same card
+		// with a counter to make sure we only do this 4 times
+
+		return trashOptions();
 	}
 
-	private void trashCard(Player player) {
+	protected List<ActionEnum<Player>> trashOptions() {
+		if (trashedSoFar > 3 || lastTrashWasNONE())
+			return emptyList;
+
 		List<CardType> hand = new ArrayList<CardType>();
 		for (CardType c : player.getCopyOfHand()) {
 			if (!hand.contains(c))
 				hand.add(c);
 		}
-		List<ActionEnum<Player>> options = new ArrayList<ActionEnum<Player>>();
-		for (CardType card : hand) {
-			options.add(CardTypeAugment.trashCard(card, CardSink.HAND));
-		}
-		options.add(CardTypeAugment.trashCard(CardType.NONE, CardSink.HAND));
 
-		player.setState(Player.State.PURCHASING);
-		Action<Player> choice = (Action<Player>) player.getDecider().decide(player, options);
-		choice.start();
-		choice.run();
-		player.setState(Player.State.PLAYING);
+		List<ActionEnum<Player>> retValue = new ArrayList<>();
+		retValue.add(CardTypeAugment.trashCard(CardType.NONE, CardSink.HAND));
+
+		for (CardType card : hand) {
+			retValue.add(CardTypeAugment.trashCard(card, CardSink.HAND));
+		}
+		trashedSoFar++;
+
+		return retValue;
+	}
+
+	private boolean lastTrashWasNONE() {
+		List<Action<?>> executedActions = player.getExecutedActions();
+		if (executedActions.isEmpty()) return false;
+		DominionAction lastAction = (DominionAction) executedActions.get(executedActions.size()-1);
+		CardTypeAugment cta = (CardTypeAugment) lastAction.getType();
+		return cta.card == CardType.NONE;
+	}
+
+
+	@Override
+	public DominionAction followUpAction() {
+		DominionAction retValue = new ChapelFollowOnAction(player,this);
+		return retValue;
+	}
+	@Override
+	public void reset() {
+		trashedSoFar = 0;
+		player = null;
+		this.setRef("");
+	}
+
+	@Override
+	public Chapel clone(DominionGame newGame) {
+		Chapel retValue = (Chapel) super.clone(newGame);
+		if (player != null) retValue.player = newGame.getPlayer(player.getNumber());
+		retValue.trashedSoFar = trashedSoFar;
+		return retValue;
+	}
+
+}
+
+class ChapelFollowOnAction extends DominionAction {
+
+	private Chapel chapel;
+
+	public ChapelFollowOnAction(Player player, Chapel card) {
+		super(player, new CardTypeList(new ArrayList<CardType>()));
+		chapel = card;
+	}
+
+	@Override
+	public ChapelFollowOnAction clone(Player newPlayer) {
+		List<Card> newCards = newPlayer.getCardsWithRef(chapel.getRef());
+		if (newCards.size() != 1)
+			throw new AssertionError("Expect exactly one card with reference of " + chapel.getRef() + " and found " + newCards.size());
+		return new ChapelFollowOnAction(newPlayer, (Chapel) newCards.get(0));
+	}
+
+	@Override
+	public void doStuff() {
+		possibleOptions = chapel.trashOptions();
+		followUpAction = new ChapelFollowOnAction(player, chapel);
+	}
+
+	@Override
+	public String toString() {
+		return "Follow-on CHAPEL";
+	}
+
+	@Override
+	public ActionEnum<Player> getType() {
+		return new CardTypeAugment(CardType.CHAPEL, CardSink.HAND, CardSink.HAND, ChangeType.NOCHANGE);
 	}
 }
+
